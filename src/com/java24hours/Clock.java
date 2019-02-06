c8439b1cebd2de5467c257d886d9c780459b40df
rahulk <Rahul.k.mbox1@gmail.com>
2019-02-06 13:55:46 -0500

    working time functionality...need to test realtime features

--- src/com/java24hours/Clock.java
+++ src/com/java24hours/Clock.java
@@ -23,6 +23,58 @@ class Clock {
         }
         
         //begin time by showing minutes
-        System.out.println("It's ");
+        System.out.print("It's ");
+        if (minute != 0){
+            System.out.print(minute + " " );
+            System.out.print((minute !=1) ? "minutes": "minute");
+            System.out.print(" past");
+        }
+        //displaying the hour
+        System.out.print(" ");
+        System.out.print((hour>12) ? (hour -12) : hour);
+        System.out.print(" o'clock on ");
+        
+        //display time of month
+        switch(month){
+            case 1:
+                System.out.print("January");
+                break;
+            case 2:
+                System.out.print("February");
+                break;
+            case 3:
+                System.out.print("March");
+                break;
+            case 4:
+                System.out.print("April");
+                break;
+            case 5:
+                System.out.print("May");
+                break;
+            case 6:
+                System.out.print("June");
+                break;
+            case 7:
+                System.out.print("July");
+                break;
+            case 8:
+                System.out.print("August");
+                break;
+            case 9:
+                System.out.print("September");
+                break;
+            case 10:
+                System.out.print("October");
+                break;
+            case 11:
+                System.out.print("November");
+                break;
+            case 12:
+                System.out.print("December");
+                break;
+        }
+        
+        //Display the date and Year
+        System.out.println(" "+ day+ ", " + year + "." );
     }
 }
