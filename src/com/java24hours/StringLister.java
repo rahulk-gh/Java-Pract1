7b92a3f5714395f582898c45a814663049dd129c
rahulk <Rahul.k.mbox1@gmail.com>
2019-03-05 12:40:49 -0500

    clarifications on arraylist

--- src/com/java24hours/StringLister.java
+++ src/com/java24hours/StringLister.java
@@ -12,9 +12,12 @@ public class StringLister {
     //constructor
     public StringLister(String[] moreNames){
         ArrayList<String> list = new ArrayList<String> ();
+        //we dont know the total number of names that we may pass through
+        //args, so array list works better than array. 
         for (int i = 0; i< names.length; i++){
             list.add(names[i]);
         }
+        //morenames is from args
         for(int i = 0; i< moreNames.length; i++){
             list.add(moreNames[i]);
         }
@@ -23,7 +26,10 @@ public class StringLister {
             System.out.println(name);
         }
     }
+    //main method
     public static void main(String[] args){
         StringLister lister = new StringLister(args);
-    }
+    }//not sure why lister is showing up as unused. Maybe bad coding practice
+    //and this was just a demo
+    //another constructor without args? review constructors
 }
